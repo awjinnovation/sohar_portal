372bfbfcae37a67e51512c7ba523a1fa938cbb5d
Da Svilu 3 <yousuf@yousuf-abdullah.me>
2025-10-10 21:45:27 +0400

    fk constraints 2

--- database/migrations/2025_09_21_100000_drop_workshop_tables.php
+++ database/migrations/2025_09_21_100000_drop_workshop_tables.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,12 +12,27 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Disable foreign key checks temporarily
+        $driver = DB::getDriverName();
+        if ($driver === 'mysql') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        } elseif ($driver === 'sqlite') {
+            DB::statement('PRAGMA foreign_keys = OFF;');
+        }
+
         // Drop workshop-related tables
         Schema::dropIfExists('workshop_schedules');
         Schema::dropIfExists('workshop_registrations');
         Schema::dropIfExists('cultural_workshops');
         Schema::dropIfExists('craft_demonstration_schedules');
         Schema::dropIfExists('photography_tips');
+
+        // Re-enable foreign key checks
+        if ($driver === 'mysql') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        } elseif ($driver === 'sqlite') {
+            DB::statement('PRAGMA foreign_keys = ON;');
+        }
     }
 
     /**
